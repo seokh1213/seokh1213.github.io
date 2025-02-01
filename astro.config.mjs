4f7ec6e617cbcfe888f9a8b237700e4eb383536b
홍석현 <seokh1213@gmail.com>
2025-02-01 21:08:33 +0900

    feat: alpine js 로컬 설치로 변경

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import alpinejs from "@astrojs/alpinejs";
+
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), alpinejs()],
   site: "https://seokh1213.github.io",
 });
