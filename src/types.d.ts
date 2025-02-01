47f9bfffac52a38f64d4f3e80f71a20103fa253d
홍석현 <seokh1213@gmail.com>
2025-02-01 15:31:52 +0900

    feat: astro 버전 업데이트

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,14 @@
 export type SocialIcon = Record<string, string | any>;
+
+export interface Content<T> {
+  data: T
+}
+
+export interface Post {
+  title: string,
+  description: string,
+  dateFormatted: string,
+  published: boolean,
+  tags: string[],
+  category: string,
+}
